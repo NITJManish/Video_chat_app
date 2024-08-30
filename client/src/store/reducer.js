789aea33236c51ae2ce5e7af9005412576194573
neera <kumarmanish150799@gmail.com>
2024-08-30 10:41:04 +0530

    setup client and server folder

---
+++ client/src/store/reducer.js
@@ -0,0 +1,18 @@
+const initState={
+    identity:''
+};
+
+
+const reducer=(state=initState,action)=>{
+    switch(action.type){
+        case 'DUMMY_ACTION':
+            return {
+                ...state
+            };
+            default:
+                return state;
+    }
+}
+
+
+export default reducer;
